ecd0edf5f0b00489e7f0ee64ec936503043e8807
Matthew Higgins <mthiggins1s@semo.edu>
2025-04-07 15:50:32 -0500

    refactor: move button logic to a standalone component and update header template

---
+++ 01-starting-project/src/app/shared/button/button.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  // selector has been changed from 'app-button' to this selector:
+  // with this, we tell Angular that any button elements, ANYWHERE in the application that also has the appButton atribute on it, should be controlled by THIS component.
+  selector: 'button[appButton]',
+  standalone: true,
+  imports: [],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.css'
+})
+export class ButtonComponent {}
